376dccdbac935204d9b9cf71934da31fb1563e30
BIJIT DEB <bijitdeb0703@gmail.com>
2025-06-11 01:35:13 +0600

    home page new section add

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,35 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Slide from './Slide';
+
+const Slider = () => {
+    return (
+        <>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: true,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>helo</SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
+
+export default Slider;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import Contact from '../../components/Contact/Contact';
+import Faq from '../../components/Faq/Faq';
+import Slider from '../../components/Slider/Slider';
 
 const Home = () => {
     return (
-        <div>
-            home
-        </div>
+        <>
+            <Slider />
+            <div className="container mx-auto px-4 py-8">
+                <h1 className="text-3xl font-bold text-center mb-4">Welcome to Our Event</h1>
+                <p className="text-center mb-8">Discover exciting events and book your spot today!</p>
+
+                {/* event card  */}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+
+                </div>
+            </div>
+
+            <Faq />
+            <Contact />
+        </>
     );
 };
 
